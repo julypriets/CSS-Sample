97817fc836e383bdb601ab53ddbfc620ab4214da
Juliana Prieto <jprietoarcila@gmail.com>
2018-07-02 22:31:07 -0500

    NavBar agregada

--- css/index.js
+++ css/index.js
@@ -8,6 +8,18 @@ $(function(){
     showModal(idImg, srcImg);
   });
   hideModal();
+  // Navbar
+  $('.fa-bars').click(function(){
+    $('nav ul li').addClass("show");
+    $('.fa-times').css("display","block");
+    $('.fa-bars').css("display","none");
+  });
+
+  $('.fa-times').click(function(){
+    $('nav ul li').removeClass("show");
+    $('.fa-times').css("display","none");
+    $('.fa-bars').css("display","block");
+  });
 });
 
   function showModal(idImg, srcImg){
